9148f4e681fe3fab912c2c8ab87ed845b4feb79c
Matthew Stadelman <stadelmanma@gmail.com>
2020-12-16 21:13:59 -0500

    Improve handling of bad temperature readings
    This also sends all "messages" to stderr which I think is
    best practices.

--- temperature-monitor.sh
+++ temperature-monitor.sh
@@ -17,12 +17,36 @@ w1_dir="/sys/bus/w1/devices"
 counter_file="temperature-monitor.counter"
 
 
-read_temperature() {
+echo_err() {
+    1>&2 echo "$@"
+}
+
+
+read_sensor() {
     # Reads a temperature value from the sensor's data bus
     #
     # args: sensor_uuid
-    # output: temperature in degC
-    cat "$w1_dir/$1/w1_slave"  | tail -n1 | awk -F '=' '{print $2/1000}'
+    # output: temperature in degC, or blank if reading failed
+    cat "$w1_dir/$1/w1_slave"  | cut -d= -f 2 | tr -s '\n ' ',' | awk -F, '{if ($2=="YES") print $3/1000}'
+}
+
+
+read_temperature() {
+    # Reads a temperature value from the sensor's data bus
+    #
+    # args: sensor_uuid, max_attempts
+    # output: temperature in degC, or blank if reading failed
+    temp=""
+    attempt=0
+    while [ -z "$temp" ] && [ $attempt -lt $2 ]; do
+        temp="$(read_sensor $1)"
+        attempt=$(($attempt + 1))
+        if [ -z "$temp" ]; then
+            echo_err "[ERROR] Failed to read temperature for sensor $1 ($attempt/$2 attempts)"
+            sleep 1  # sleep for a second to try and let the problem pass
+        fi
+    done
+    echo "$temp"
 }
 
 
@@ -60,7 +84,7 @@ plot_data() {
 # exist set counter to 0
 if [ ! -f "$counter_file" ]; then
     counter='0'
-    echo "No counter file found at '$counter_file', setting counter=0"
+    echo_err "[WARNING] No counter file found at '$counter_file', setting counter=0"
 else
     counter=$(cat "$counter_file")
 fi
@@ -68,36 +92,43 @@ echo $(($counter + $interval )) >  "$counter_file"
 
 
 # take readings
-temp="$(read_temperature $sensor1_uuid)"
-sensor1_tempf=$(awk "BEGIN {print $temp*1.8 + 32}")
+temp="$(read_temperature $sensor1_uuid 10)"
 timestamp="$(date '+%Y-%m-%d %H:%M:%S %Z')"
-save_temperature "$sensor1_uuid" "$sensor1_name" "$timestamp" "$temp"
-echo "Saved $sensor1_name temperature reading of ${temp}C at $timestamp"
+if [ ! -z "$temp" ]; then
+    sensor1_tempf=$(awk "BEGIN {print $temp*1.8 + 32}")
+    save_temperature "$sensor1_uuid" "$sensor1_name" "$timestamp" "$temp"
+    echo_err "[INFO] Saved $sensor1_name temperature reading of ${temp}C at $timestamp"
+else
+    sensor1_tempf=""
+fi
 
-temp="$(read_temperature $sensor2_uuid)"
-sensor2_tempf=$(awk "BEGIN {print $temp*1.8 + 32}")
+temp="$(read_temperature $sensor2_uuid 10)"
 timestamp="$(date '+%Y-%m-%d %H:%M:%S %Z')"
-save_temperature "$sensor2_uuid" "$sensor2_name" "$timestamp" "$temp"
-echo "Saved $sensor2_name temperature reading of ${temp}C at $timestamp"
-
+if [ ! -z "$temp" ]; then
+    sensor2_tempf=$(awk "BEGIN {print $temp*1.8 + 32}")
+    save_temperature "$sensor2_uuid" "$sensor2_name" "$timestamp" "$temp"
+    echo_err "[INFO] Saved $sensor2_name temperature reading of ${temp}C at $timestamp"
+else
+    sensor2_tempf=""
+fi
 
 # regenerate plots, this is expensive so we don't do it every time
 if [ "$(($counter % 300))" = "0" ]; then  # 5 minutes
-    echo "Recreating 24h plot..."
+    echo_err "[INFO] Recreating 24h plot..."
     plot_data "$(date -d "$(date) - 24 hours" '+%Y-%m-%d %H:%M:%S %Z')" 10 2 '%m/%d %H:%M' > "$output_path/temperature-24h.png"
 fi
 if [ "$(($counter % 3600))" = "0" ]; then  # 1 hour
-    echo "Recreating 7d plot..."
+    echo_err "[INFO] Recreating 7d plot..."
     plot_data "$(date -d "$(date) - 7 days" '+%Y-%m-%d %H:%M:%S %Z')" 20 5 '%m/%d' > "$output_path/temperature-7d.png"
-    echo "Uploading 7d plot to B2 bucket '$b2_bucket'..."
+    echo_err "[INFO] Uploading 7d plot to B2 bucket '$b2_bucket'..."
     ~/.local/bin/b2 upload-file "$b2_bucket" "$output_path/temperature-7d.png" temperature-7d.png
 fi
 if [ "$(($counter % 21600))" = "0" ]; then  # 6 hours
-    echo "Recreating 1y plot..."
+    echo_err "[INFO] Recreating 1y plot..."
     plot_data "$(date -d "$(date) - 1 year" '+%Y-%m-%d %H:%M:%S %Z')" 100 10 '%m/%d' > "$output_path/temperature-1y.png"
-    echo "Uploading 1y plot to B2 bucket '$b2_bucket'..."
+    echo_err "[INFO] Uploading 1y plot to B2 bucket '$b2_bucket'..."
     ~/.local/bin/b2 upload-file "$b2_bucket" "$output_path/temperature-1y.png" temperature-1y.png
-    echo "Uploading database to B2 bucket '$b2_bucket'..."
+    echo_err "[INFO] Uploading database to B2 bucket '$b2_bucket'..."
     ~/.local/bin/b2 upload-file "$b2_bucket" "$temperature_db" temperature-readings.db
 fi
 
@@ -110,7 +141,7 @@ sed "s/%sensor2_name%/$sensor2_name/" | \
 sed "s/%sensor2_temp%/$sensor2_tempf/" > "$output_path/index.html"
 
 if [ "$(($counter % 900))" = "0" ]; then  # 15 minutes
-    echo "Uploading 24h plot and HTML page to B2 bucket '$b2_bucket'..."
+    echo_err "[INFO] Uploading 24h plot and HTML page to B2 bucket '$b2_bucket'..."
     ~/.local/bin/b2 upload-file "$b2_bucket" "$output_path/index.html" temperature.html
     ~/.local/bin/b2 upload-file "$b2_bucket" "$output_path/temperature-24h.png" temperature-24h.png
 fi
